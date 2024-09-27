d51249b09491e36ac43d7c9034a0d87f1a564c73
Kiran.Yesley <kirany@nycstudents.edu>
2024-09-27 11:47:15 -0400

    last class commit

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,9 +4,9 @@ public class MyArrays{
     int[] array1 = new  int[]{7,8,9};
     int[] array2 = new  int[]{};
     int[] array3 = new  int[]{0,0, 2,5,6};
-    System.out.print("Original array:" + array1 + "New array" + returnCopy(array1)+ "Are the values equal:" + arrayToString(array1).equals(arrayToString(returnCopy(array1))));
-    System.out.print("Original array:" + array2 + "New array" + returnCopy(array2)+ "Are the values equal:" + arrayToString(array2).equals(arrayToString(returnCopy(array2))));
-    System.out.print("Original array:" + array3 + "New array" + returnCopy(array3)+ "Are the values equal:" + arrayToString(array3).equals(arrayToString(returnCopy(array3))));
+    System.out.println("Original array:" + array1 + " New array" + returnCopy(array1)+ " Are the values equal:" + arrayToString(array1).equals(arrayToString(returnCopy(array1))));
+    System.out.println("Original array:" + array2 + " New array" + returnCopy(array2)+ " Are the values equal:" + arrayToString(array2).equals(arrayToString(returnCopy(array2))));
+    System.out.println("Original array:" + array3 + " New array" + returnCopy(array3)+ " Are the values equal:" + arrayToString(array3).equals(arrayToString(returnCopy(array3))));
 
   }
   public static String arrayToString(int[] nums){
@@ -18,7 +18,7 @@ public class MyArrays{
         result += ", ";
       }
     }
-    return result
+    return result;
   }
   public static int[] returnCopy(int[]ary)
   { int[] ans = new int[ary.length];
@@ -26,6 +26,6 @@ public class MyArrays{
     {
       ans[i] = ary[i];
     }
-    return ans; 
+    return ans;
   }
 }
