cb52d1a38a9ab7957ad58fda93a8072f81f81230
Kiran.Yesley <kirany@nycstudents.edu>
2024-10-01 14:47:57 -0400

    class commit replaceNegative

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -18,6 +18,14 @@ public static void main(String[] args){
   int [][] arr4 = {{1,2},{3,4},{5,6},{7,8}};
   System.out.println("Expected: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]  Obtained: " + arrToString(swapRC(arr1)));
   System.out.println("Expected: [[1, 3, 5, 7], [2, 4, 6, 8]]  Obtained: " + arrToString(swapRC(arr4)));
+// test cases for replaceNegative
+  int [][] arr5 = {{1,-2,-3},{4,-5,6},{-7,9}};
+  int [][] arr6 = {{-1,-2,7},{4,5,6},{}, {-1}};
+  replaceNegative( arr5)
+  replaceNegative( arr6)
+  System.out.println("Expected: [[1, 0, 0], [4, 1, 6], [0, 9]]  Obtained: " + arrToString(arr5);
+  System.out.println("Expected: [[1,0,7], [4,5,6], [], [0]]  Obtained: " + arrToString(arr6);
+
 
 }
 //2. Copy your arrToString method from before.
@@ -83,5 +91,36 @@ public static int[][] swapRC(int[][]nums){
   }
   return arr;
 }
+//3. Modify a given 2D array of integer as follows:
+//Replace all the negative values:
+//-When the row number is the same as the column number replace
+//that negative with the value 1
+//-All other negatives replace with 0
+public static void replaceNegative(int[][] vals){
+  for (int i = 0; i < vals.length ; i++)
+  {
+    for(int j =0 ; j< vals[i]; j++)
+    {
+      if (vals[i][j] < 0){
+        if (i == j){
+          vals[i][j] = 1;
+        }
+        else {
+          vals[i][j] = 0;
+        }
+      }
+    }
+  }
+}
+
+//4. Make a copy of the given 2d array.
+//When testing : make sure that changing the original does NOT change the copy.
+//DO NOT use any built in methods that "copy" an array.
+//You SHOULD write a helper method for this.
+//If you don't see a good way to do that, you should stop and look at prior methods.
+public static int[][] copy(int[][] nums){
+  return null;//placeholder so it compiles
+}
+
 
 }
