1fc555e88f7faedb8a7924c995b56b3b7bb16aac
Kiran.Yesley <kirany@nycstudents.edu>
2024-10-31 11:40:48 -0400

    updated main with the sum and printing word functions

--- 2024-10-31-Advent/Main.java
+++ 2024-10-31-Advent/Main.java
@@ -11,6 +11,20 @@ class Main {
 
       while(input.hasNext()){
         System.out.println(input.next());
+
+      Scanner sc1 = new Scanner("This is a bunch of words");
+      Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+
+      while(sc1.hasNext()){
+        System.out.println(sc1.next());
+      }
+      double sum = 0;
+
+      while(sc2.hasNextDouble()){
+        sum+=sc2.nextDouble();//read all the numbers and add to sum
+      }
+
+      System.out.println(sum);
       }
     }
   }
