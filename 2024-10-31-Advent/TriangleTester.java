c173714a93309a35be9a14c8b2234e73d1b5f2a7
Kiran.Yesley <kirany@nycstudents.edu>
2024-11-04 11:47:14 -0500

    added main

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -3,6 +3,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriangleTester{
+  public static void main(String[] args){
+    System.out.println(isTriangle(3,4,5) + "should print true");
+  }
+
 
   public boolean isTriangle(Integer a , Integer b, Integer c){
     boolean x = (a + b < c);
