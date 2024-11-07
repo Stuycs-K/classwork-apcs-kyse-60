1db0f07a558e58cdd8829957e87a2b75a34ced37
Kiran Yesley <kyesley@nycstudents.net>
2024-11-07 16:24:30 -0500

    testing what constructors work

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,7 @@
+  //Step 8 answer
+  //calling getName from a bird object uses the overridden method of getName method. However
+  //calling getName from a Animal object uses the original getName method. 
+
 public class Driver{
   public static void main(String[] args){
     Animal a = new Animal("buzz",5,"bee");
@@ -7,8 +11,13 @@ public class Driver{
     b.speak();
     System.out.println(b.getName() + " should return something with Mighty");
     System.out.println(a.getName() + " should not return something with Mighty");
+
+    //Testing what can work:
+    Animal a1 = new Animal("ok",4,"me");
+    Bird b1 = new Bird("yes", 8, "you", 5.0 , "blue");
+    //Bird b2 = new Animal("ok1",41,"me1"); - animal cannot be converted to bird
+    Animal a2 = new Bird("yes0", 80, "you0", 50.0 , "blue0");
   }
 
-  //Step 8 answer
-  //calling getName from a bird object uses the overridden method of getName method. However, calling getName from a Animal object uses the original getName method. 
+ 
 }
