421974b35caa8ff98ead10988b7b0d6d43c9a24b
Kiran.Yesley <kirany@nycstudents.edu>
2024-11-07 11:40:19 -0500

    made bird object + accessors and overided speak

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,8 @@ public class Driver{
   public static void main(String[] args){
     Animal a = new Animal("buzz",5,"bee");
     a.speak();
+
+    Bird b = new Bird("tweet", 2, "bob", 12.0, "brown");
+    b.speak();
   }
 }
