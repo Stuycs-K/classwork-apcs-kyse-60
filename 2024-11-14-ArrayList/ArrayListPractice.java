6eabe442bd401bc74dddc969d543dbfc987239c1
Kiran Yesley <kyesley@nycstudents.net>
2024-11-14 17:01:19 -0500

    finished classwork

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -34,12 +34,12 @@ public class ArrayListPractice {
 //a[0], b[0], a[1], b[1]...
 //If one list is longer than the other, just attach the remaining values to the end
   ArrayList<String> ans = new ArrayList<String>(a.size() + b.size());
-  for (int i = 0; i < Math.min(a.size() - 1, b.size() - 1); i++){
+  for (int i = 0; i <= Math.min(a.size() - 1, b.size() - 1); i++){
     ans.add(a.get(i));
     ans.add(b.get(i));
   }
   if (a.size() > b.size()) {
-    for(int i= b.size() -1 ; i < a.size() ; i++) ans.add(a.get(i));
+    for(int i= b.size() - 1 ; i < a.size()  ; i++) ans.add(a.get(i));
   }
   if (b.size() > a.size()) {
     for(int i= a.size() - 1; i < b.size() ; i++) ans.add(b.get(i));
