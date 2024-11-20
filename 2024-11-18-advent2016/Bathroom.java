ec25164c67ef5e7ab218606904bf468fda687f34
Kiran Yesley <kyesley@nycstudents.net>
2024-11-20 12:29:59 -0500

    finished BAthroom (day 2 part 1)

--- 2024-11-18-advent2016/Bathroom.java
+++ 2024-11-18-advent2016/Bathroom.java
@@ -1,18 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Bathroom{
     public static void main(String[] args) {
         System.out.println(Decode("input.txt."));
       }
+
+      static int x = 2;
+      static int y = 3;
+
     
-    public static int Decode(String filename){
+    public static String Decode(String filename){
         try {
           File file = new File(filename);
           Scanner a = new Scanner(file);
-
+          int current = 5;
+          String ans = "";
+          while(a.hasNextLine()){
+            String str = a.nextLine();
+            for (int i = 0; i < str.length(); i++){
+                if (str.substring(i,i+1).equals("U") && y != 0) y -= 3;
+                if (str.substring(i,i+1).equals("D") && y != 6) y += 3;
+                if (str.substring(i,i+1).equals("R") && x != 3) x++;
+                if (str.substring(i,i+1).equals("L") && x!= 1) x--;
+            }
+            int add = x + y;
+            ans += add ; 
+          }
           a.close();
-          return null;
+          return ans;
         }catch (FileNotFoundException ex) {
           System.out.println("File not found" + filename);
-          return -1;
+          return "no";
       }
     }
 }
