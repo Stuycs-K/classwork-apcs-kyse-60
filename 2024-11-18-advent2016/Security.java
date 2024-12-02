453a8bd40f811e7042ddcf861056ede7ef366d85
Kiran Yesley <kyesley@nycstudents.net>
2024-12-02 11:47:22 -0500

    demoed Random files

--- 2024-11-18-advent2016/Security.java
+++ 2024-11-18-advent2016/Security.java
@@ -11,35 +11,61 @@ public class Security{
         System.out.println(sumSector("input.txt."));
       }
     
+    public static String combine(String[] a){
+        String ans ="";
+        for (int i = 0; i < a.length - 1; i++){
+            ans += a[i];
+        }
+        return ans;
+    }
+
+    public static int maxoflist(int[] list){
+      int max = 0;
+      int maxindex = 0;
+      for(int i = 0; i < list.length; i++){
+        if (list[i] > max){
+          max = list[i];
+          maxindex = i;
+        } 
+      }
+      return maxindex;
+    }
+    
+    public static String findMax5(String a){
+      String result = "";
+      int[] count = new int[26];
+        for(int j = 0; j <a.length(); j++){
+            for( int i = 0; i < 26; i ++){
+              char character = (char) (i + 97);
+              if (a.charAt(j) == character) count[i]++;
+             }
+        }
+      for(int x = 0; x < 5;x++){
+        char ans = (char)(maxoflist(count) + 'a');
+        result += ans;
+        count[maxoflist(count)] = 0;
+      }
+    return result; 
+  }
+    
+    
     public static int sumSector(String filename){
         try {
           File file = new File(filename);
           Scanner a = new Scanner(file);
+          int answer = 0;
           while(a.hasNextLine()){
             String[]dir = a.nextLine().split("-");
             int sectorID = Integer.parseInt((dir[dir.length-1]).substring(0,3));
             String checksum = (dir[dir.length-1]).substring(4,9);
-            
-            char[] characters= combine(dir).toCharArray();     // this includes the [asdkasdas] code make sure u change it
-            Arrays.sort(characters);
-            String [][] results = new String[26][2];            // ASCII EXISTS make a 1d array of ints[26] 0 corresponds to 'a'
-            for(int i = 0; characters.length > 0; i++){
-                int count = 1;
-                while(characters[i] == (characters[i+1])){
-                    i ++;
-                }
-                results[i][0] = 
-                String[] arry = { counter + "" , characters[0] + ""};
-                results.add(arry);
-                characters = Arrays.copyOfRange(characters, i + 1, characters.length);
-            }
+            if (findMax5(combine(dir)).compareTo(checksum) == 0) answer += sectorID;
 
-            }
-            return null; 
+            } 
             a.close();
+            return answer;
           }catch (FileNotFoundException ex) {
           System.out.println("File not found" + filename);
-          return null;
+          return 0;
       }
     }
 }
